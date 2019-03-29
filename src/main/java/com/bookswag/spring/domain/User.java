6c66136a369815c8e9104f070509302a3e314750
book_swag <its.bookswag@gmail.com>
2019-03-29 22:27:02 +0900

    [CH-1-1-2] First Dao

---
+++ src/main/java/com/bookswag/spring/domain/User.java
@@ -0,0 +1,14 @@
+package com.bookswag.spring.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+    String id;
+    String name;
+    String password;
+}
