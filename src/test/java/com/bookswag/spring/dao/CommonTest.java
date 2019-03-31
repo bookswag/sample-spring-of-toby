5012f0c904102c6a92696524ac009f1e62b17683
book_swag <its.bookswag@gmail.com>
2019-04-01 08:18:05 +0900

    [CH1-6] Test to get beans from Spring context

---
+++ src/test/java/com/bookswag/spring/dao/CommonTest.java
@@ -0,0 +1,23 @@
+package com.bookswag.spring.dao;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+
+public class CommonTest {
+    @Test
+    public void getBeansIdForCheckingSingleton() {
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+
+        UserDao firstDao = context.getBean("userDao", UserDao.class);
+        UserDao secondDao = context.getBean("userDao", UserDao.class);
+
+        System.out.println(firstDao);
+        System.out.println(secondDao);
+        assertThat(secondDao, is(firstDao));
+    }
+}
