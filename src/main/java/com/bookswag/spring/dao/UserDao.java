f12eaa1625c0620c68dc7eb69d4ac3d5def819e3
book_swag <its.bookswag@gmail.com>
2019-04-01 08:22:09 +0900

    [CH1-6-1] Typical Singleton on UserDao (no build)

--- src/main/java/com/bookswag/spring/dao/UserDao.java
+++ src/main/java/com/bookswag/spring/dao/UserDao.java
@@ -7,9 +7,17 @@ import java.sql.*;
 
 public class UserDao {
     private ConnectionMaker connectionMaker;
+    private static UserDao INSTANCE;
 
-    public UserDao(ConnectionMaker connectionMaker) {
+    private UserDao(ConnectionMaker connectionMaker) {
         this.connectionMaker = connectionMaker;
+    }
+
+    public static synchronized UserDao getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new UserDao(???);
+            return INSTANCE;
+        }
     }
 
     public void add(User user) throws ClassNotFoundException, SQLException {
