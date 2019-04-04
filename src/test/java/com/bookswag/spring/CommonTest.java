cf8fdf4cf4f6af821c8e1c199f0d737a0eda3976
book_swag <its.bookswag@gmail.com>
2019-04-04 21:57:28 +0900

    [CH2-4-1] Remove previous env & Add slf4j as log4j

--- src/test/java/com/bookswag/spring/CommonTest.java
+++ src/test/java/com/bookswag/spring/CommonTest.java
@@ -1,25 +1,33 @@
 package com.bookswag.spring;
 
-import com.bookswag.spring.dao.DaoFactory;
 import com.bookswag.spring.dao.UserDao;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
-
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations="/applicationContext.xml")
 public class CommonTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CommonTest.class);
+
+    @Autowired
+    private ApplicationContext context;
+
     @Test
     public void getBeansIdForCheckingSingleton() {
-        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-
         UserDao firstDao = context.getBean("userDao", UserDao.class);
         UserDao secondDao = context.getBean("userDao", UserDao.class);
 
-        System.out.println(firstDao);
-        System.out.println(secondDao);
+        LOGGER.info("first : {}", firstDao);
+        LOGGER.info("second : {}", secondDao);
         assertThat(secondDao, is(firstDao));
     }
 }
--- src/test/java/com/bookswag/spring/dao/UserDaoTest.java
+++ src/test/java/com/bookswag/spring/dao/UserDaoTest.java
@@ -1,13 +1,13 @@
 package com.bookswag.spring.dao;
 
 import com.bookswag.spring.domain.User;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -29,6 +29,7 @@ import static org.junit.Assert.assertThat;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations="/applicationContext.xml")
 public class UserDaoTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserDaoTest.class);
     @Autowired
     private ApplicationContext context;
 
@@ -39,6 +40,9 @@ public class UserDaoTest {
         dao = context.getBean("userDao", UserDao.class);
         dao.deleteAll();
         assertThat(dao.getCount(), is(0));
+
+        LOGGER.info("this.context : {}",this.context);
+        LOGGER.info("this : {}", this);
     }
 
     @Test
