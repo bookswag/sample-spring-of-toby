9bfb9144d10f620f0d7b9b1636f71de9b0bf5c4e
book_swag <its.bookswag@gmail.com>
2019-04-10 00:37:39 +0900

    [CH-0] JdbcTemplate with lamda

--- src/main/java/com/bookswag/spring/dao/UserDao.java
+++ src/main/java/com/bookswag/spring/dao/UserDao.java
@@ -2,11 +2,7 @@ package com.bookswag.spring.dao;
 
 import com.bookswag.spring.domain.User;
 import lombok.NoArgsConstructor;
-import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCreator;
-import org.springframework.jdbc.core.ResultSetExtractor;
-import org.springframework.jdbc.core.RowMapper;
 
 import javax.sql.DataSource;
 import java.sql.*;
@@ -20,53 +16,34 @@ public class UserDao {
     }
 
     public void add(final User user) {
-        this.jdbcTemplate.update(new PreparedStatementCreator() {
-            @Override
-            public PreparedStatement createPreparedStatement(Connection c) throws SQLException {
-                PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
-                ps.setString(1, user.getId());
-                ps.setString(2, user.getName());
-                ps.setString(3, user.getPassword());
-                return ps;
-            }
+        this.jdbcTemplate.update(c -> {
+            PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
+            ps.setString(1, user.getId());
+            ps.setString(2, user.getName());
+            ps.setString(3, user.getPassword());
+            return ps;
         });
     }
 
     public User get(final String id) {
         return this.jdbcTemplate.queryForObject("select * from users where id = ?", new Object[]{id},
-            new RowMapper<User>() {
-                @Override
-                public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-                    User user = new User();
-                    user.setId(rs.getString("id"));
-                    user.setName(rs.getString("name"));
-                    user.setPassword(rs.getString("password"));
-                    return user;
-                }
+            (ResultSet rs, int rowNum) -> {
+                User user = new User();
+                user.setId(rs.getString("id"));
+                user.setName(rs.getString("name"));
+                user.setPassword(rs.getString("password"));
+                return user;
         });
     }
 
     public void deleteAll() {
-        this.jdbcTemplate.update(new PreparedStatementCreator() {
-            @Override
-            public PreparedStatement createPreparedStatement(Connection c) throws SQLException {
-                return c.prepareStatement("delete from users");
-            }
-        });
+        this.jdbcTemplate.update(c -> c.prepareStatement("delete from users"));
     }
 
     public int getCount() {
-        return this.jdbcTemplate.query(new PreparedStatementCreator() {
-            @Override
-            public PreparedStatement createPreparedStatement(Connection c) throws SQLException {
-                return c.prepareStatement("select count(*) from users");
-            }
-        }, new ResultSetExtractor<Integer>() {
-            @Override
-            public Integer extractData(ResultSet rs) throws SQLException, DataAccessException {
-                rs.next();
-                return rs.getInt(1);
-            }
-        });
+        return this.jdbcTemplate.query(
+            c -> c.prepareStatement("select count(*) from users"),
+            (rs) -> { rs.next();
+                return rs.getInt(1); });
     }
 }
