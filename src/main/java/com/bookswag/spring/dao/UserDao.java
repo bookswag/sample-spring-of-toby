4bfde57520e1351853208226d6cd1ee358dbbe08
book_swag <its.bookswag@gmail.com>
2019-04-10 00:29:57 +0900

    [CH3-6-3] Change to org.springframework.jdbc.core.JdbcTemplate

--- src/main/java/com/bookswag/spring/dao/UserDao.java
+++ src/main/java/com/bookswag/spring/dao/UserDao.java
@@ -1,34 +1,72 @@
 package com.bookswag.spring.dao;
 
-import com.bookswag.spring.database.JdbcContext;
 import com.bookswag.spring.domain.User;
 import lombok.NoArgsConstructor;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.RowMapper;
 
 import javax.sql.DataSource;
 import java.sql.*;
 
 @NoArgsConstructor
 public class UserDao {
-    private JdbcContext jdbcContext;
+    private JdbcTemplate jdbcTemplate;
 
     public void setDataSource(DataSource dataSource) {
-        this.jdbcContext = new JdbcContext();
-        this.jdbcContext.setDataSource(dataSource);
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
-    public void add(final User user) throws SQLException {
-        jdbcContext.executeAddSql("insert into users(id, name, password) values(?,?,?)", user);
+    public void add(final User user) {
+        this.jdbcTemplate.update(new PreparedStatementCreator() {
+            @Override
+            public PreparedStatement createPreparedStatement(Connection c) throws SQLException {
+                PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
+                ps.setString(1, user.getId());
+                ps.setString(2, user.getName());
+                ps.setString(3, user.getPassword());
+                return ps;
+            }
+        });
     }
 
-    public User get(final String id) throws SQLException {
-        return jdbcContext.executeGetUserSql("select * from users where id = ?", id);
+    public User get(final String id) {
+        return this.jdbcTemplate.queryForObject("select * from users where id = ?", new Object[]{id},
+            new RowMapper<User>() {
+                @Override
+                public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+                    User user = new User();
+                    user.setId(rs.getString("id"));
+                    user.setName(rs.getString("name"));
+                    user.setPassword(rs.getString("password"));
+                    return user;
+                }
+        });
     }
 
-    public void deleteAll() throws SQLException {
-        jdbcContext.executeSql("delete from users");
+    public void deleteAll() {
+        this.jdbcTemplate.update(new PreparedStatementCreator() {
+            @Override
+            public PreparedStatement createPreparedStatement(Connection c) throws SQLException {
+                return c.prepareStatement("delete from users");
+            }
+        });
     }
 
-    public int getCount() throws SQLException {
-        return jdbcContext.executeGetCountSql("select count(*) from users");
+    public int getCount() {
+        return this.jdbcTemplate.query(new PreparedStatementCreator() {
+            @Override
+            public PreparedStatement createPreparedStatement(Connection c) throws SQLException {
+                return c.prepareStatement("select count(*) from users");
+            }
+        }, new ResultSetExtractor<Integer>() {
+            @Override
+            public Integer extractData(ResultSet rs) throws SQLException, DataAccessException {
+                rs.next();
+                return rs.getInt(1);
+            }
+        });
     }
 }
