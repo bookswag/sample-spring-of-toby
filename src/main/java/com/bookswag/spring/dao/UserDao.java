a435457d563e9b6629d803dcf6f692f0e613c636
book_swag <its.bookswag@gmail.com>
2019-04-11 08:36:23 +0900

    [CH3-6-5] Extract reusable callback

--- src/main/java/com/bookswag/spring/dao/UserDao.java
+++ src/main/java/com/bookswag/spring/dao/UserDao.java
@@ -3,15 +3,24 @@ package com.bookswag.spring.dao;
 import com.bookswag.spring.domain.User;
 import lombok.NoArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 import javax.sql.DataSource;
 import java.sql.*;
 import java.util.List;
-import com.google.common.collect.Lists;
 
 @NoArgsConstructor
 public class UserDao {
     private JdbcTemplate jdbcTemplate;
+
+    // RowMapper callback object is stateless
+    private static RowMapper<User> userRowMapper = (ResultSet rs, int rowNum) -> {
+        User user = new User();
+        user.setId(rs.getString("id"));
+        user.setName(rs.getString("name"));
+        user.setPassword(rs.getString("password"));
+        return user;
+    };
 
     public void setDataSource(DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
@@ -22,25 +31,11 @@ public class UserDao {
     }
 
     public User get(final String id) {
-        return this.jdbcTemplate.queryForObject("select * from users where id = ?", new Object[]{id},
-            (ResultSet rs, int rowNum) -> {
-                User user = new User();
-                user.setId(rs.getString("id"));
-                user.setName(rs.getString("name"));
-                user.setPassword(rs.getString("password"));
-                return user;
-        });
+        return this.jdbcTemplate.queryForObject("select * from users where id = ?", new Object[]{id}, userRowMapper);
     }
 
     public List<User> getAll() {
-        return this.jdbcTemplate.query("select * from users order by id",
-            (ResultSet rs, int rowNum) -> {
-                User user = new User();
-                user.setId(rs.getString("id"));
-                user.setName(rs.getString("name"));
-                user.setPassword(rs.getString("password"));
-                return user;
-            });
+        return this.jdbcTemplate.query("select * from users order by id", userRowMapper);
     }
 
     public void deleteAll() {
